68cfc826ca18c22ebd956fde39d1fcb0323c1c6e
zoeurk <zoeurk@gmail.com>
2021-01-15 18:47:05 +0100

    update

--- json2txt.c
+++ json2txt.c
@@ -418,7 +418,7 @@ struct json *to_json(int fd){
 		fprintf(stderr, "double quote non fermee a l'offset: %lu\n", parts[hug-1].offset);
 		exit(EXIT_FAILURE);
 	}
-	if(parts[hug-1].len != 0 && parts[hug-1].array != 0){
+	if(parts[hug-1].len != 0 || parts[hug-1].array != 0){
 		if(parts[hug-1].array > 0)
 			fprintf(stderr, "Trop de '[' ouverts a l'offset %lu.\n", parts[hug-1].offset);
 		else
