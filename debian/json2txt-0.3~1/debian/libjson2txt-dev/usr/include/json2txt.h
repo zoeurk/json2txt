91e81c9b2f04107ce7057ca2d1e4e81aa490c6f9
zoeurk <zoeurk@gmail.com>
2025-08-09 17:28:17 +0200

    update

---
+++ debian/json2txt-0.3~1/debian/libjson2txt-dev/usr/include/json2txt.h
@@ -0,0 +1,140 @@
+/*read(), STD*_FILENO*/
+#include <unistd.h>
+/*memset, memcpy, strcpy, strcat, strcmp*/
+#include <string.h>
+#include <strings.h>
+/*err, errx, warnx*/
+#include <err.h>
+#include <errno.h>
+/*sprintf, printf*/
+#include <stdio.h>
+/*malloc, calloc(), realloc()*/
+#include <stdlib.h>
+extern int json_err;
+#define INIT_JSON(BUFSIZE, ALLOC_SIZE, CHARS, buffer) { 0, BUFSIZE, ALLOC_SIZE, CHARS, buffer, NULL, 0, 0, 1, NULL, NULL, 0, 0, 0 }
+enum TYPE{
+	ARRAY = 1,
+	PAIR = 2
+};
+enum VAL{
+	VOID = 0,
+	STRING = 1,
+	INT = 2,
+	WARN = 4,
+	WARN_NBR = 8,
+	WARN_BOOL = 16,
+	SET = 32
+};
+enum KIND{
+	NEW,
+	SUB,
+	NEXT
+};
+struct json_parser{
+	int file;
+	unsigned int buflen;
+	size_t stock_buf;
+	char *chars;
+	char *buffer;
+	char *buf;
+	size_t len;
+	ssize_t r_len;
+	ssize_t offset;
+	char *pstock;
+	char *stock;
+	size_t stock_size;
+	size_t depth;
+	size_t data_size;
+};
+struct json{
+	enum TYPE type;
+	int t_val;
+	struct{
+		union{
+			size_t index;
+			char *key;
+		}name;
+		char *value;
+	}value;
+	struct json *next;
+	struct json *prev;
+	struct json *sub;
+	struct json *up;
+};
+struct json_new_lst{
+	struct json *j;
+	int json_err;
+	#if __WORDSIZE != 32
+		int ___;
+	#endif
+	struct json_new_lst *prev;
+	struct json_new_lst *next;
+};
+struct fn{
+	size_t index;
+	ssize_t offset;
+	int key;
+	int value;
+	int c_end;
+	int err;
+	struct {
+		union{
+			int (*get_it)(struct json_parser *, ...);
+			int (*get_str)(struct json_parser *);
+			int (*get_int)(struct json_parser *, int);
+		}get;
+		union{
+			void (*up_it)(struct json **,...);
+			void (*up_from_array)(struct json **, struct json_parser *, struct fn *);
+			void (*up_from_pair)(struct json **, struct json_parser *);
+			void (*up_int)(struct json **, struct json_parser *, struct fn *);
+		}up;
+		union{
+			void (*up_string)(struct json_parser *, ...);
+			void (*up_int)(struct json_parser *, ...);
+			void (*up_from_string)(struct json_parser *, struct fn *);
+			void (*up_int_from_array)(struct json_parser *, struct json *, struct fn *);
+			void (*up_int_from_pair)(struct json_parser *, struct json *);
+		}structure;
+		union{
+			int (*do_it)(struct json_parser *, ...);
+			int (*read_with_this)(struct json_parser *, struct json **, struct fn **, struct json_new_lst **);
+			int (*do_errors)(struct json_parser *, struct json **, struct fn **);
+		}do_it;
+	}fns;
+	struct fn *next;
+	struct fn *prev;
+};
+struct json *go_first(struct json *j);
+ssize_t read_fn(struct json_parser *p);
+void readchar(ssize_t *offset, char **str, const char *not);
+int json_errors(struct json_parser *p, struct json **j, struct fn **f);
+int json_int_test(char *stock, char *pstock);
+int json_bool_test(char *stock);
+void *allocstr(char **buffer, size_t lentoadd);
+void *json_create(struct json **j, enum KIND kind, enum TYPE type);
+
+int get_str(struct json_parser *p);
+int get_int(struct json_parser *p, int init);
+void up_str_from_array(struct json **j, struct json_parser *p, struct fn *f);
+void up_str_from_pair(struct json **j, struct json_parser *p);
+void up_from_bool(struct json **j, struct json_parser *p, struct fn *f);
+void up_from_int(struct json **j, struct json_parser *p, struct fn *f);
+void up_structure_from_int_pair(struct json_parser *p, struct json *j);
+void up_structure_from_int_array(struct json_parser *p, struct json *j, struct fn *f);
+void up_structure_from_str(struct json_parser *p, struct fn *f);
+
+int starting(struct json_parser *p, struct json **j, struct fn **f, struct json_new_lst **nj);
+int array(struct json_parser *p, struct json **j, struct fn **f, struct json_new_lst **nj);
+int pair(struct json_parser *p, struct json **j, struct fn **f, struct json_new_lst **nj);
+
+int json_sort_asc(struct json **j, int warn_only);
+int json_sort_dsc(struct json **j, int warn_only);
+int json_test_sort(struct json **j, int (*sorting)(struct json **, int), int warn_only);
+int json_test(struct json **j, int warn_only);
+
+void json_print(struct json *j, size_t space, char *sep, char c_sp, size_t count);
+int json2txt(struct json *j, char *string, int empty);
+
+void json_destroy(struct json **j);
+
--- debian/json2txt_0.3~1-1.dsc
+++ debian/json2txt_0.3~1-1.dsc
@@ -15,26 +15,26 @@ Package-List:
  libjson2txt deb utils optional arch=any
  libjson2txt-dev deb libdevel optional arch=any
 Checksums-Sha1:
- a8d5bdfced1e31704e81c71bfdf211002fe79ca1 156660 json2txt_0.3~1.orig.tar.xz
+ 836e9ca23a3e5c20d67bf32f57f6c7f6be60f19d 135332 json2txt_0.3~1.orig.tar.xz
  a661d8fff864d6c0a3d398b7535f847e5719524f 7924 json2txt_0.3~1-1.debian.tar.xz
 Checksums-Sha256:
- 237e2f3e781864136e787667eac367ba601688f378e0e744f8c3cf448970f64a 156660 json2txt_0.3~1.orig.tar.xz
+ 4c251a3a5c0195205a19018fa1615f36efac391d061cf267e876ff64c85f4215 135332 json2txt_0.3~1.orig.tar.xz
  f801c1e7f3825cb5fd96cb1fe122332519f0e1b3a0611705899ce679a64302c9 7924 json2txt_0.3~1-1.debian.tar.xz
 Files:
- eb23a8785d4b22cc590d528c0999c6ec 156660 json2txt_0.3~1.orig.tar.xz
+ 868b543f833477ec7e31179e14b96acc 135332 json2txt_0.3~1.orig.tar.xz
  b9ed928d95b6e0c647420c744abd78fe 7924 json2txt_0.3~1-1.debian.tar.xz
 
 -----BEGIN PGP SIGNATURE-----
 
-iQHFBAEBCgAvFiEEgnh2EWglF7jJm3HO5SbKMk2fAI4FAmiXP3YRHHpvZXVya0Bn
-bWFpbC5jb20ACgkQ5SbKMk2fAI5mEgwAivYO7Ybp4WlwGQzIe61DbU5I/+0l/mXy
-1kcYZScesW85sSRbTBt2cIbEg7yIs7D3HTWJku3NWSM7WJp2DPC6B8bTIheVYeoY
-1Pe1XoH2Snu96H9gM3BvCCATe0ZlzYb1q6Ifslyh4WbON0UWXpEGsrX8SwwjYDa6
-3MynaLziN29b9Qzzl0ymI4HUhMrnQUvrvMcMWKF/Wxk/eQ0jwgyZlrWoT8Oapg0M
-ddwGLVHPpYsBa3F4caX4L0EtdhsgaZDpTEJ62JI9Sq1kCo2Ulb9Va7Pu36tCqzoF
-vLL2jfVpAd25o1mOfXEPNXKx6lopt4/KIaVdIJb89ZKc/Oe/6IMQ3iYLfM7/C4M6
-i5+qW9kKY6VKyKtidnASLGPFS9kEDl8DlrcnrBa17yhsBvKW8tRe/OdCKAlqpFxl
-bNwo5JkDlMZDRK1kPtgKGc1fISHP4W/pgyB3lsHOg+CSMnZg+wiPfbYBPreHAfca
-90lWHOFFe6ARPigmg1VJUz4lOrecIFZN
-=57On
+iQHFBAEBCgAvFiEEgnh2EWglF7jJm3HO5SbKMk2fAI4FAmiXaNkRHHpvZXVya0Bn
+bWFpbC5jb20ACgkQ5SbKMk2fAI6f9wv+IFi+82aLQw8P/QNCDWIv0HakmBU6XQnA
+I9TgfwyIcnM5T8GwCv4GmIXG3DCNKcD93DGxl+RAsRVhJwx+NzlAebaqRiL4Yshg
+WqA6f9teJ4ccZzwo4UqN/YIgNUUYoOmevXXuhXEKIcavj5k+GM7squthoIFPNa5c
+QK2TsjcRNSqouaymymgsAveO6DBBnSp5kM6to4wGr7rMLCr4wp5K7DRPizBt00NB
+YM5dOaOOjdZ30/mYp9z8nXXugY8AcC8Yg7Pdy5POTNIR3WVV/GdprjwVKECqwtiZ
+1KCkun1ES4SMCGoeDpwLGBeMh3GkD0zU0YWuTXSCvxS1E6q1n3MvYhhFjvCZUzCW
+z9onwhPhxh9ngeIIN+6tgLAAoIQWBDB4Xrr1kHz6vI9q/sRbJJpm6N7EoTOqTEWb
+tOd2fheUpJm1t6Scw0aOGplExdNBeFSXhvSemq1AGx35ahguRpN1oxI4Y1+aafhM
+MRROtcPyHk3rlLp3PddvsdsKVPw+kJHQ
+=wf8Y
 -----END PGP SIGNATURE-----
